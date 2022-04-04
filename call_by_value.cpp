87987f01d5f4f8bfcdae91db9c0016b02c650a80
yuki.kobayashi <ykobayashi134@gmail.com>
2022-04-04 19:42:01 +0000

    call by value

---
+++ call_by_value.cpp
@@ -0,0 +1,32 @@
+// C++の引数渡しは全て値渡しなので、データを変更したいならポインタで参照渡し
+#include <iostream.h>
+
+void ChangeToShohwa(int x)
+{
+    if(1926 < x && x < 1989)
+        x -= 1925;
+    else
+        x = 0;
+}
+
+void Shohwa()
+{
+    int nYear;
+
+    cout << "西暦を入力して下さい > ";
+    cin >> nYear;
+
+    ChangeToShohwa(nYear);
+    if(nYear)
+        cout << "その年は昭和 " << nYear
+             << " 年です。" << endl;
+    else
+        cout << "その年は昭和ではありません。" << endl;
+}
+
+int main()
+{
+    Shohwa();
+    Shohwa();
+    return 0;
+}
