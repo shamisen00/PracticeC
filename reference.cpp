ff99bb82495d7211fb847644b0a3309379b9428f
yuki.kobayashi <ykobayashi134@gmail.com>
2022-04-05 21:45:24 +0000

    参照

---
+++ reference.cpp
@@ -0,0 +1,20 @@
+#include <iostream.h>
+
+int main()
+{
+    int a;
+    int& r = a;
+
+    a = 0;
+    cout << "a  = " << a << endl
+         << "r  = " << r << endl << endl;
+
+    r = 100;
+    cout << "a  = " << a << endl
+         << "r  = " << r << endl << endl;
+
+    cout << "&a = " << (unsigned int)&a << endl
+         << "&r = " << (unsigned int)&r << endl;
+
+    return 0;
+}
